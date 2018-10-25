748fada75d68b84fc523150f64878bfbbdd7afad
Juan Francisco Crespo Galán <dethon@usal.es>
2018-10-25 14:27:01 +0200

    Added some usage examples

---
+++ examples/stop_and_restart.cpp
@@ -0,0 +1,34 @@
+#include "../ThreadPool.hpp"
+#include <iostream>
+#include <string>
+#include <chrono>
+
+using namespace std::chrono_literals;
+int task(int a)
+{
+	std::cout << std::to_string(a) + " thread start\n";
+	std::this_thread::sleep_for(1s);
+	std::cout << std::to_string(a) + " thread end\n";
+	return 0;
+}
+
+int main()
+{
+	auto&& pool{ ThreadPool{std::thread::hardware_concurrency()} };
+	for (auto i = 0u; i < 20u; i++)
+	{
+		pool.enqueue(&task, i);
+	}
+
+	std::this_thread::sleep_for(500ms);
+	pool.stop();
+	std::this_thread::sleep_for(5s);
+	pool.init(1u);
+	std::this_thread::sleep_for(5s);
+	pool.stop();
+	std::this_thread::sleep_for(5s);
+	pool.init(std::thread::hardware_concurrency());	
+	
+	// ThreadPool's destructor uses softStop, so all queued tasks are executed before exiting the program.
+	return 0;
+}
