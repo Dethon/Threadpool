fffadaca87a12ed1e11ea18a38c1ac5d6bb0cdc4
Juan Francisco Crespo Galán <jf_crespo@outlook.com>
2019-02-28 20:26:16 +0100

    Made the example's output a bit clearer.

--- examples/stop_and_restart.cpp
+++ examples/stop_and_restart.cpp
@@ -14,19 +14,25 @@ int task(int a)
 
 int main()
 {
+	std::cout << "NORMAL STARTUP:\n";
 	auto pool = ThreadPool{ std::thread::hardware_concurrency() };
-	for (auto i = 0u; i < 20u; i++)
+	for (auto i = 0u; i < 60u; i++)
 	{
 		pool.enqueue(&task, i);
 	}
-
 	std::this_thread::sleep_for(500ms);
+	std::cout << "\nSTOPPING...\n";
 	pool.stop();
+	std::cout << "STOPPED\n";
 	std::this_thread::sleep_for(5s);
+	std::cout << "\nRESTARTING WITH ONE THREAD:\n";
 	pool.start(1u);
 	std::this_thread::sleep_for(5s);
+	std::cout << "\nSTOPPING...\n";
 	pool.stop();
+	std::cout << "STOPPED\n";
 	std::this_thread::sleep_for(5s);
+	std::cout << "\nRESTARTING WITH ALL THREADS:\n";
 	pool.start(std::thread::hardware_concurrency());	
 	
 	// ThreadPool's destructor uses softStop, so all queued tasks are executed before exiting the program.
