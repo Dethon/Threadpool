88f836eb923c1d8c8d899e91b75b30dd09a579cb
Juan Francisco Crespo Galán <JF_Crespo@outlook.es>
2021-02-26 03:18:13 +0100

    Rename examples/basic_usage.cpp to code/examples/basic_usage.cpp

---
+++ code/examples/basic_usage.cpp
@@ -0,0 +1,25 @@
+﻿#include "../ThreadPool.hpp"
+#include <iostream>
+#include <string>
+
+int task(int a, float c)
+{
+	std::cout << std::to_string(a) + " --- " + std::to_string(c) + "\n";
+	return 0;
+}
+
+int main()
+{
+	auto pool = ThreadPool{ std::thread::hardware_concurrency() };
+	auto futures = std::vector<std::future<int>>{};
+	for (auto i = 0u; i < 20u; i++)
+	{
+		futures.push_back(pool.enqueue(&task, i, i/100.0f));
+	}
+	for (const auto &fut : futures)
+	{
+		fut.wait();
+	}
+
+	return 0;
+}
